a83f15a4c41c0d8102c495b42eeb3b1eee722480
crisleyhguimaraes <crisleyhguimaraes@gmail.com>
2022-09-13 14:03:11 -0300

    config do ambiente mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container} >
+      <Text>
+        Teste
+        </Text>
+
+        < Button title='Botão' />
+
+      <StatusBar style="auto" backgroundColor= '#919191'/>
+    </View>
+  );
+}
+
+interface ButtonProps{
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
